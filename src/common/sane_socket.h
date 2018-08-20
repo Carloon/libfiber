f6aedb8dcdfb627b257182768bfea9a4f9194dcb
Carlton Mills <Carlton.Mills@runtimeverification.com>
2018-08-20 11:51:35 -0500

    added changed to include <../../fiber...>

--- src/common/sane_socket.h
+++ src/common/sane_socket.h
@@ -1,7 +1,7 @@
 #ifndef SANE_SOCKET_INCLUDE_H
 #define SANE_SOCKET_INCLUDE_H
 
-#include <fiber/libfiber.h>
+#include <../../fiber/libfiber.h>
 
 int is_listen_socket(socket_t fd);
 int getsocktype(socket_t fd);
