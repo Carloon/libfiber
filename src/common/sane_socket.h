6c337a1e638dca0fee62f57f98554dea56efe96e
Carlton Mills <Carlton.Mills@runtimeverification.com>
2018-08-20 12:10:18 -0500

    Inclined libfiber.h

--- src/common/sane_socket.h
+++ src/common/sane_socket.h
@@ -1,7 +1,20 @@
 #ifndef SANE_SOCKET_INCLUDE_H
 #define SANE_SOCKET_INCLUDE_H
 
+#if 0  /* kcc couldn't follow the path */
 #include <../../include/fiber/libfiber.h>
+#endif
+#ifndef LIB_FIBER_INCLUDE_H
+#define LIB_FIBER_INCLUDE_H
+
+#include "fiber_base.h"
+#include "fiber_lock.h"
+#include "fiber_event.h"
+#include "fiber_sem.h"
+#include "fiber_hook.h"
+#include "fiber_channel.h"
+
+#endif
 
 int is_listen_socket(socket_t fd);
 int getsocktype(socket_t fd);
